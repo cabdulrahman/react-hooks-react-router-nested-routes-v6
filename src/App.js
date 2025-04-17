d9d57c862a4467e00318520dc00b642d48380c4b
abdulrahman <abdulrahmanibnimohammed@gmail.com>
2025-04-17 09:39:11 +0300

    added nested routing into home component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
+import { Outlet } from "react-router-dom";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
       <header>
-        <NavBar
+        <NavBar />
       </header>
+      <Outlet />
     </>
   );
-};
+}
 
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,30 @@
 import { useState, useEffect } from "react";
 import UserCard from "../components/UserCard";
-import NavBar from "../components/NavBar";
+import { Outlet } from "react-router-dom";
 
 function Home() {
   const [users, setUsers] = useState([]);
 
-  useEffect(() =>{
+  useEffect(() => {
     fetch("http://localhost:4000/users")
-      .then(r => r.json())
-      .then(data => setUsers(data))
-      .catch(error => console.error(error));
+      .then((r) => r.json())
+      .then((data) => setUsers(data))
+      .catch((error) => console.error(error));
   }, []);
-  
-  const userList = users.map(user =>{
-    return <UserCard key={user.id} user={user}/>;
+
+  const userList = users.map((user) => {
+    return <UserCard key={user.id} user={user} />;
   });
 
   return (
     <>
-      <header>
-        <NavBar />
-      </header>
       <main>
         <h1>Home!</h1>
+        <Outlet />
         {userList}
       </main>
     </>
   );
-};
+}
 
 export default Home;
